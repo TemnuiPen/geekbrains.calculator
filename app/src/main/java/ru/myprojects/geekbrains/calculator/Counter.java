5667e3f8ecf40e7db9b04d667b6487067a8ae995
TemnuiPen <maltsev0017@gmail.com>
2021-06-21 22:22:17 +0300

    First working version

--- app/src/main/java/ru/myprojects/geekbrains/calculator/Counter.java
+++ app/src/main/java/ru/myprojects/geekbrains/calculator/Counter.java
@@ -10,26 +10,47 @@ public class Counter {
     StringBuffer stringBuffer = new StringBuffer();
 
 
+    public double getResult() {
+        return result;
+    }
+
     public void setLine(String line) {
         stringBuffer.append(line);
     }
 
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    public void setValueTwo(double valueTwo) {
+        this.valueTwo = valueTwo;
+    }
+
+    public void setValueOne(double valueOne) {
+        this.valueOne = valueOne;
+    }
+
     void parseStr () {
         String line = stringBuffer.toString();
-        String[] arrSplit = line.split(" ",3);
-        valueOne = Double.parseDouble(arrSplit[1]);
-        valueTwo = Double.parseDouble(arrSplit[3]);
+        String[] arrSplit = line.split("\\s",3);
+        valueOne = Double.parseDouble(arrSplit[0]);
+        valueTwo = Double.parseDouble(arrSplit[2]);
 
-        switch (arrSplit[2]) {
+        switch (arrSplit[1]) {
             case "+":
                 result = valueOne + valueTwo;
+                break;
             case "-":
                 result = valueOne - valueTwo;
+                break;
             case "*":
                 result = valueOne * valueTwo;
+                break;
             case "/":
                 result = valueOne / valueTwo;
+                break;
         }
+        stringBuffer.delete(0 , stringBuffer.length());
     }
 
 }
--- app/src/main/java/ru/myprojects/geekbrains/calculator/MainActivity.java
+++ app/src/main/java/ru/myprojects/geekbrains/calculator/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.PersistableBundle;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,13 +32,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnDot;
     Button btnEquals;
     Counter counter;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        Counter counter = new Counter();
+        counter = new Counter();
     }
 
     @Override
@@ -97,6 +99,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnDivide = findViewById(R.id.btnDevide);
         btnDot = findViewById(R.id.btnDot);
         btnEquals = findViewById(R.id.btnEquals);
+        textView = findViewById(R.id.tvText);
 
         btnZero.setOnClickListener(this);
         btnOne.setOnClickListener(this);
@@ -123,36 +126,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.btnOne:
                 counter.setLine("1 ");
+                textView.setText("1 ");
+                break;
             case R.id.btnTwo:
                 counter.setLine("2 ");
+                textView.setText("2 ");
+                break;
             case R.id.btnThree:
                 counter.setLine("3 ");
+                textView.setText("3 ");
+                break;
             case R.id.btnFour:
                 counter.setLine("4 ");
+                textView.setText("4 ");
+                break;
             case R.id.btnFive:
                 counter.setLine("5 ");
+                textView.setText("5 ");
+                break;
             case R.id.btnSix:
                 counter.setLine("6 ");
+                textView.setText("6 ");
+                break;
             case R.id.btnSeven:
                 counter.setLine("7 ");
+                textView.setText("7 ");
+                break;
             case R.id.btnEight:
                 counter.setLine("8 ");
+                textView.setText("8 ");
+                break;
             case R.id.btnNine:
                 counter.setLine("9 ");
+                textView.setText("9 ");
+                break;
             case R.id.btnZero:
                 counter.setLine("0 ");
+                textView.setText("0 ");
+                break;
             case R.id.btnPlus:
                 counter.setLine("+ ");
+                textView.setText("+ ");
+                break;
             case R.id.btnMinus:
                 counter.setLine("- ");
+                textView.setText("- ");
+                break;
             case R.id.btnMultiply:
                 counter.setLine("* ");
+                textView.setText("* ");
+                break;
             case R.id.btnDevide:
                 counter.setLine("/ ");
+                textView.setText("/ ");
+                break;
             case R.id.btnDot:
                 counter.setLine(".");
+                textView.setText(".");
+                break;
             case R.id.btnEquals:
                 counter.parseStr();
+
+                textView.setText("");
+                String str = String.valueOf(counter.getResult());
+                textView.setText(str);
+
+                counter.setValueOne(0);
+                counter.setValueTwo(0);
+                counter.setResult(0);
+                break;
         }
     }
 }
